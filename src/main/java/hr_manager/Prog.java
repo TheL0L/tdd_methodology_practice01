b8f18ac5a2fe782419164441b790c6481c9c6057
Sergei <sergeiyakima1@gmail.com>
2024-02-09 18:32:24 +0200

    added a sorting function, and updated main()

--- src/main/java/hr_manager/Prog.java
+++ src/main/java/hr_manager/Prog.java
@@ -1,15 +1,38 @@
 package hr_manager;
 
+import java.util.function.Supplier;
+
 public class Prog
 {
     public static void main(String[] args)
     {
-        System.out.println("bananas");
-        System.out.println(AddInt(5, 5));
+        final int count = 10_000;
+        Worker[] workers = WorkerGenerator.generateWorkers(count);
+
+        long startTime = System.currentTimeMillis();
+        customSort(workers);
+        long endTime = System.currentTimeMillis();
+
+        long executionTime = endTime - startTime;
+        System.out.println("Execution Time: " + executionTime + "ms.");
     }
 
-    public static int AddInt(int a, int b)
-    {
-        return a + b + 1;
+    public static <T extends Comparable<T>> void customSort(T[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (array[i - 1].compareTo(array[i]) > 0) {
+                    // Swap elements if they are in the wrong order
+                    T temp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
     }
 }
