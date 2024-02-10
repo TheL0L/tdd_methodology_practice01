b6e4b71979cb034d13f76331cda5e95f3375d93d
Sergei <sergeiyakima1@gmail.com>
2024-02-10 16:14:11 +0200

    updated the sorting function to return the result

--- src/main/java/hr_manager/Prog.java
+++ src/main/java/hr_manager/Prog.java
@@ -1,38 +1,41 @@
 package hr_manager;
 
-import java.util.function.Supplier;
+import java.util.Arrays;
 
 public class Prog
 {
     public static void main(String[] args)
     {
-        final int count = 10_000;
+        final int count = 1_000;
         Worker[] workers = WorkerGenerator.generateWorkers(count);
 
         long startTime = System.currentTimeMillis();
-        customSort(workers);
+        var arr = customSort(workers);
         long endTime = System.currentTimeMillis();
 
         long executionTime = endTime - startTime;
         System.out.println("Execution Time: " + executionTime + "ms.");
     }
 
-    public static <T extends Comparable<T>> void customSort(T[] array) {
+    public static <T extends Comparable<T>> T[] customSort(T[] array) {
         int n = array.length;
+        T[] sortedArray = Arrays.copyOf(array, n);
         boolean swapped;
 
         do {
             swapped = false;
             for (int i = 1; i < n; i++) {
-                if (array[i - 1].compareTo(array[i]) > 0) {
+                if (sortedArray[i - 1].compareTo(sortedArray[i]) > 0) {
                     // Swap elements if they are in the wrong order
-                    T temp = array[i - 1];
-                    array[i - 1] = array[i];
-                    array[i] = temp;
+                    T temp = sortedArray[i - 1];
+                    sortedArray[i - 1] = sortedArray[i];
+                    sortedArray[i] = temp;
                     swapped = true;
                 }
             }
             n--;
         } while (swapped);
+
+        return sortedArray;
     }
 }
